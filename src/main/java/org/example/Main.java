ce7e42d719cca23fd2cf8c9050f638f858ff719c
arxip <arxipov1989@gmail.com>
2023-06-10 16:25:30 +0300

    1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,21 @@
 package org.example;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        StringList stringList = new StringListImpl();
+        stringList.add("a1");
+        stringList.add("b2");
+        stringList.add("c3");
+        stringList.add(3, "d4");
+        System.out.println(Arrays.toString(stringList.toArray()));
+        stringList.remove(2);
+        System.out.println(Arrays.toString(stringList.toArray()));
+        stringList.clear();
+        System.out.println(stringList.size());
     }
+
 }
